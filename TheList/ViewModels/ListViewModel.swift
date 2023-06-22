98d7e023b608fa9ff4a5c7809af4993e0382f842
dongseok cho <dscho0134@gmail.com>
2023-06-21 19:52:39 -0600

    Add ListView swiftUI view and ListViewModel swift file

---
+++ TheList/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  TheList
+//
+//  Created by Dongseok Cho on 2023-06-21.
+//
+
+import Foundation
---
+++ TheList/Views/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  TheList
+//
+//  Created by Dongseok Cho on 2023-06-21.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
