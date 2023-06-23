6462024343c2a2ce45683065443f8ea27f1e95ae
SeraHan <serahan524@gmail.com>
2023-06-22 22:59:46 -0600

    Add UI features

--- TheList/Models/ItemList.swift
+++ TheList/Models/ItemList.swift
@@ -1,8 +1,14 @@
 //
-//  ItemList.swift
+//  ListItem.swift
 //  TheList
 //
-//  Created by Sera Han on 2023-06-02.
+//  Created by Sera Han on 2023-06-22.
 //
 
 import Foundation
+
+struct ItemList: Codable {
+    var id: String
+    var description: String
+    var quantity: Int
+}
